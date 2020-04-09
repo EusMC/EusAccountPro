e5f4f46203ca734843f87ffa536448bf8efec593
ElaBosak233 <Detective_XD@163.com>
2020-04-09 19:26:07 +0800

    JsonDB

--- src/main/java/cn/elabosak/eusaccountpro/database/JsonDB.java
+++ src/main/java/cn/elabosak/eusaccountpro/database/JsonDB.java
@@ -1,13 +1,23 @@
 package cn.elabosak.eusaccountpro.database;
 
+import java.io.*;
 import java.lang.reflect.Array;
+import java.lang.reflect.Member;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 import cn.elabosak.eusaccountpro.controller.AuthController;
+import com.alibaba.fastjson.JSONObject;
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.zxing.Reader;
 import org.bukkit.entity.Player;
 
 public class JsonDB extends Database {
     Gson gson = new Gson();
+
     @Override
     public String getSecretKey(UUID uuid) {
         return null;
@@ -15,7 +25,19 @@ public class JsonDB extends Database {
 
     @Override
     public boolean updatePlayer(UUID uuid, String secretKey) {
-        String jsonObject = gson.toJson(uuid,secretKey);
+        String uuid_string = uuid.toString();
+        JSONObject jsonObject = new JSONObject();
+        Object pj = jsonObject.put(uuid_string, secretKey);
+        String playerJson = jsonObject.toJSONString(pj);
+        File file = new File("/players/"+uuid_string+".json");
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            JSONObject.writeJSONString(fileOutputStream,playerJson);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 }
