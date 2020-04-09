2c8658787d9bba35784c574661898b29b8f0ee1f
AlanRichard <alan_richard@eumc.cc>
2020-04-09 15:20:27 +0800

    AuthController sketch

--- src/main/java/cn/elabosak/eusaccountpro/controller/AuthController.java
+++ src/main/java/cn/elabosak/eusaccountpro/controller/AuthController.java
@@ -1,6 +1,9 @@
 package cn.elabosak.eusaccountpro.controller;
 
 import cn.elabosak.eusaccountpro.EusAccountPro;
+import org.bukkit.entity.Player;
+
+import static cn.elabosak.eusaccountpro.utils.Authenticator.getTOTPCode;
 
 /**
  * Designed for player managing
@@ -10,5 +13,37 @@ public class AuthController {
 
     public AuthController(EusAccountPro instance) {
         plugin = instance;
+    }
+
+    /**
+     * Register player with TOTP secret key
+     * @param player
+     * @param secretKey
+     * @return
+     */
+    public boolean register(Player player, String secretKey) {
+
+    }
+
+    /**
+     * Get player's TOTP secret key
+     * @param player
+     * @return null: player not registered
+     */
+    public String getSecretKey(Player player) {
+        return null;
+    }
+
+    /**
+     * Verify whether the input code from player is valid or not
+     * @param player
+     * @param code
+     * @return
+     */
+    public boolean verify(Player player, String code) {
+        String secretKey = getSecretKey(player);
+        if (secretKey == null) return false; // player not registered
+
+        return code.equals(getTOTPCode(secretKey));
     }
 }
