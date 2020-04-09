3899ec40ec554061e99733045583372356d00c30
AlanRichard <alan_richard@eumc.cc>
2020-04-09 15:12:17 +0800

    Code clean up

---
+++ src/main/java/cn/elabosak/eusaccountpro/controller/AuthController.java
@@ -0,0 +1,14 @@
+package cn.elabosak.eusaccountpro.controller;
+
+import cn.elabosak.eusaccountpro.EusAccountPro;
+
+/**
+ * Designed for player managing
+ */
+public class AuthController {
+    EusAccountPro plugin;
+
+    public AuthController(EusAccountPro instance) {
+        plugin = instance;
+    }
+}
